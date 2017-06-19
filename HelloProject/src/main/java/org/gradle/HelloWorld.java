3e4e9504332b373db0a5734380e3bac785be11b1
Seung Cheol Paik <pscheol@gmail.com>
2017-06-20 01:05:35 +0900

    hello git study

--- HelloProject/src/main/java/org/gradle/HelloWorld.java
+++ HelloProject/src/main/java/org/gradle/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("world's end dancehall");
-		System.out.println("Hello dance world");
+		System.out.println("Hello dance worlder");
 	}
 }
