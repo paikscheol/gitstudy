b021ad9b5293ff3f05201f3bc827500863396b70
Seung Cheol Paik <pscheol@gmail.com>
2017-06-20 01:09:28 +0900

    Update HelloWorld.java

--- HelloProject/src/main/java/org/gradle/HelloWorld.java
+++ HelloProject/src/main/java/org/gradle/HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld {
 		System.out.println("world's end dancehall");
 		System.out.println("Hello dance worlder");
 		System.out.println("Hello dance worlds");
-
+		System.out.println("Hello dance worlds!!!!!");	
 	}
 }
